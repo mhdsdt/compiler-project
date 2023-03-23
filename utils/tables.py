3d0e464a0b55e2f3714811366874845efc6246aa
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2023-03-23 17:13:16 +0330

    Update tables.py

--- utils/tables.py
+++ utils/tables.py
@@ -10,8 +10,9 @@ class TokensTable:
     def export(self):
         with open(ROOT_DIR + 'tokens.txt', 'w') as tokens_file:
             for i, line_tokens in enumerate(self.tokens):
-                tokens_as_string = [f'({token[0]}, {token[1]})' for token in line_tokens]
-                tokens_file.write(f'{i + 1}.\t' + ' '.join(tokens_as_string) + '\n')
+                if line_tokens:
+                    tokens_as_string = [f'({token[0]}, {token[1]})' for token in line_tokens]
+                    tokens_file.write(f'{i + 1}.\t' + ' '.join(tokens_as_string) + ' \n')
 
 
 class SymbolsTable:
@@ -21,17 +22,23 @@ class SymbolsTable:
     def export(self):
         with open(ROOT_DIR + 'symbol_table.txt', 'w') as symbols_file:
             for i, symbol in enumerate(self.symbols):
-                symbols_file.write(f'{i + 1}.\t {symbol}\n')
+                symbols_file.write(f'{i + 1}.\t{symbol}\n')
 
 
 class ErrorsTable:
     def __init__(self, errors: list):
-        self.errors = errors
+        self.errors = {}
+        for error in errors:
+            line_num, error_str, message = error
+            if self.errors.get(line_num):
+                self.errors[line_num].append((error_str, message))
+            else:
+                self.errors[line_num] = [(error_str, message)]
 
     def export(self):
         with open(ROOT_DIR + 'lexical_errors.txt', 'w') as errors_file:
             if not self.errors:
-                return errors_file.write('There is no lexical error.\n')
-            for error in self.errors:
-                line_num, error_str, message = error
-                errors_file.write(f'{line_num}.\t {error_str} - {message}\n')
+                return errors_file.write('There is no lexical error.')
+            for line_num, errors in self.errors.items():
+                errors_as_string = ' '.join([f'({error[0]}, {error[1]})' for error in errors])
+                errors_file.write(f'{line_num}.\t{errors_as_string} \n')
