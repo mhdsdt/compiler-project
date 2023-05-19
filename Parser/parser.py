87a59b606d62a305fcf3bbb66860b107e25d8f0d
MohammadMow <mmowlavi2002@gmail.com>
2023-05-19 22:41:38 +0330

    fix some parts

--- Parser/parser.py
+++ Parser/parser.py
@@ -51,7 +51,6 @@ class Parser:
             lexeme = self.last_token[0]
         else:
             lexeme = self.last_token[1]
-        print(top_of_stack)
         return self.table[(top_of_stack.name, lexeme)]
 
     def _update_stack(self, ):
@@ -61,11 +60,8 @@ class Parser:
 
         if isinstance(last_stmt, NonTerminal):
             rhs = self.get_rhs_from_table(last_stmt)
-            if rhs:
-                print('->', " ".join([str(term) for term in rhs]))
             if not rhs or rhs[0].name == 'SYNCH':
-                pass
-                # self.handle_panic(last_stmt)
+                self.handle_panic(last_stmt)
             else:
                 self.extend_stack(last_stmt, rhs)
         elif isinstance(last_stmt, Terminal):
@@ -73,8 +69,10 @@ class Parser:
                 lexeme = self.last_token[0]
             else:
                 lexeme = self.last_token[1]
-            if last_stmt.name != lexeme:
-                self.errors.append((self.scanner.get_current_line(), '', f'syntax error, missing {self.last_token[1]}'))
+            if lexeme == '$':
+                Terminal('$', parent=self.root)
+            elif last_stmt.name != lexeme:
+                self.errors.append((self.scanner.get_current_line(), '', f'syntax error, missing {lexeme}'))
             elif self.stack:
                 last_stmt.name = self.last_token
                 self.last_token = self.get_next_token()
@@ -84,21 +82,20 @@ class Parser:
         self.last_token = self.get_next_token()
         while self.stack:
             self._update_stack()
-
         self.export_parse_tree()
         self.export_syntax_errors()
 
     def export_parse_tree(self):
-        print('--------- export_parse_tree')
         for node in PreOrderIter(self.root):
             if node.name == 'EPSILON':
                 node.name = 'epsilon'
             elif type(node.name) is tuple:
                 node.name = f'({node.name[0]}, {node.name[1]})'
-            print(node.name)
         with open(ROOT_DIR + 'parse_tree.txt', 'w', encoding='utf-8') as f:
+            string = ''
             for pre, _, node in RenderTree(self.root):
-                f.write(f'{pre}{node.name}\n')
+                string += f'{pre}{node.name}\n'
+            f.write(string[:-1])
 
     def export_syntax_errors(self):
         errors_table = ErrorsTable(self.errors)
@@ -106,18 +103,25 @@ class Parser:
 
     def get_next_token(self):
         token = self.scanner.get_next_token()
-        print('------ token:', token)
         return token
 
     def handle_panic(self, last_stmt):
         print('handle panic ---->', last_stmt)
         rhs = self.get_rhs_from_table(last_stmt)
+        in_empty = False
         while not rhs:
+            in_empty = True
             self.errors.append((self.scanner.get_current_line(), '', f'syntax error, illegal {self.last_token[1]}'))
             self.last_token = self.get_next_token()
             rhs = self.get_rhs_from_table(last_stmt)
             if self.last_token[0] == TokenType.EOF.value:
                 break
+
+        if in_empty:
+            self.stack.append(last_stmt)
+
+        if rhs and rhs[0].name != 'SYNCH':
+            self.extend_stack(last_stmt, rhs)
 
         self.errors.append((self.scanner.get_current_line(), '', f'syntax error, missing {self.last_token[1]}'))
 
