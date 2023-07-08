9a8b98e0b442f2ddecc0beed70b3b9b158e0d0b2
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2023-07-08 19:45:52 +0330

    Set the number of tests for parser to 11

--- tests/parser/tester.py
+++ tests/parser/tester.py
@@ -4,4 +4,4 @@ OUTPUT_FILES = {'parse_tree.txt': 0, 'syntax_errors.txt': 0}
 
 
 if __name__ == '__main__':
-    test(output_files=OUTPUT_FILES, tests_path='testcases')
+    test(output_files=OUTPUT_FILES, tests_path='testcases', num_tests=11)
